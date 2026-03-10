34686bb43f81791ae3786bd6f8321d6aa4513b03
Sergiliy <serhey781@gmail.com>
2026-03-10 13:04:28 +0200

    feat: initialize lab6 vue project structure

---
+++ lab6/app/pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <UContainer class="py-10">
+    <UCard>
+      <template #header>
+        <h1 class="text-2xl font-bold">Nuxt Course Blog</h1>
+      </template>
+
+      <p>Стартовая страница лабораторной работы №6.</p>
+
+      <template #footer>
+        <UButton to="/blog">Перейти к блогу</UButton>
+      </template>
+    </UCard>
+  </UContainer>
+</template>
---
+++ lab6/nuxt.config.ts
@@ -0,0 +1,6 @@
+export default defineNuxtConfig({
+  compatibilityDate: "2026-03-10",
+  modules: ["@nuxt/ui"],
+  css: ["~/assets/css/main.css"],
+  devtools: { enabled: true },
+});
